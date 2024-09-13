cc1c8b989be5c9b46740d3423e7a2e081395ff4a
darinwilliams <darinjswilliams@icloud.com>
2024-09-13 11:37:40 -0500

    Initial Commit

---
+++ SoundAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SoundAnimation
+//
+//  Created by Darin Williams on 9/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SoundAnimation/SoundAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundAnimationApp.swift
+//  SoundAnimation
+//
+//  Created by Darin Williams on 9/13/24.
+//
+
+import SwiftUI
+
+@main
+struct SoundAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
